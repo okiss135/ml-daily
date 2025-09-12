7265dfbfbbd28935ca95a6020a30dc8122798be9
Oskar Kiss <oskar.h.kiss@gmail.com>
2025-09-11 22:36:37 -0500

    model for 9_11 regressor and made a bash script so i don't have to keep making the same jupyter notebook

--- model.bash
+++ model.bash
@@ -1,1 +1,7 @@
-cp "/home/oskar-kiss/repos/ml-daily/model_template.ipynb" "./model.ipynb"
+today=$(date "+%m-%d-%Y")
+current_directory=$(pwd)
+new_folder_path="/projects/${today}"
+model_path="${current_directory}${new_folder_path}/model.ipynb"
+
+mkdir "./projects/${today}"
+cp "/home/oskar-kiss/repos/ml-daily/model_template.ipynb" ${model_path}
